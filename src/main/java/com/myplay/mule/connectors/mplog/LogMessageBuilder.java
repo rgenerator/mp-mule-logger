95b35802ef8f304488026fd1d2e75939f3b4aee0
Vadim Rachko <x@vdmr.net>
2016-03-01 18:18:49 -0500

    Fix automatically generated indentation.

--- src/main/java/com/myplay/mule/connectors/mplog/LogMessageBuilder.java
+++ src/main/java/com/myplay/mule/connectors/mplog/LogMessageBuilder.java
@@ -5,26 +5,26 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 public class LogMessageBuilder {
-	
-	ArrayList<String> entries = new ArrayList<String>();
-	
-	public void addPair(String key, String value) {
-		entries.add(joinPair(key, value));
-	}
-	
-	public void addText(String text) {
-		entries.add(text);
-	}
-	
-	public String build() {
-		return StringUtils.join(entries, " ");
-	}
 
-	private String joinPair(String key, String value) {
-		return key + "=\"" + escape(value) + "\"";
-	}
+    ArrayList<String> entries = new ArrayList<String>();
 
-	private String escape(String str) {
-		return StringEscapeUtils.escapeJava(str == null ? null : str);
-	}
+    public void addPair(String key, String value) {
+        entries.add(joinPair(key, value));
+    }
+
+    public void addText(String text) {
+        entries.add(text);
+    }
+
+    public String build() {
+        return StringUtils.join(entries, " ");
+    }
+
+    private String joinPair(String key, String value) {
+        return key + "=\"" + escape(value) + "\"";
+    }
+
+    private String escape(String str) {
+        return StringEscapeUtils.escapeJava(str == null ? null : str);
+    }
 }
